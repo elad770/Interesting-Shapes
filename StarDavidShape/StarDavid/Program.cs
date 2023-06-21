0cf14769d8a719460e18dad9c7f9332db14b4d9d
elad770 <73057751+elad770@users.noreply.github.com>
2023-06-21 17:19:22 +0300

    full project

---
+++ StarDavidShape/StarDavid/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Threading.Tasks;
+using System.IO;
+using iTextSharp.text.pdf.parser;
+using static iTextSharp.text.pdf.PdfDocument;
+
+namespace StarDavid
+{
+
+
+    internal class Program
+    {
+
+        static void StarDavid(int row)
+        {
+            //darw Star-David to example:
+            //          *
+            //         * *
+            //        *   *
+            // *******************
+            //  *   *       *   *
+            //   * *         * *
+            //    *           *
+            //   * *         * *
+            //  *   *       *   *
+            // *******************
+            //        *   *
+            //         * *
+            //          *
+
+
+
+            int r = row * 4;
+            r++;
+
+            Array consoleColors = Enum.GetValues(typeof(ConsoleColor));
+            ConsoleColor randomColor = (ConsoleColor)consoleColors.GetValue(new Random().Next(consoleColors.Length));
+
+            int maxWidth = row * 6 + 1;
+            string row_star = new string('*', maxWidth);
+
+            // Create pdf file to save shape Star David
+            Document document = new Document();
+            string title = "Star of David";
+            Paragraph titleParagraph = new Paragraph(title, new Font(Font.FontFamily.COURIER, 18, Font.BOLD, BaseColor.BLACK));
+            titleParagraph.Alignment = Element.ALIGN_CENTER;
+
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream("StarDavid.pdf", FileMode.Create));
+            document.Open();
+            document.Add(titleParagraph);
+            Chunk emptyLine = new Chunk("\n");
+            document.Add(emptyLine);
+
+            int col = row_star.Length / 2 + 1;
+            int temp = col;
+            int prog = -1;
+            Console.WriteLine();
+            for (int x = 0; x < r; x++, col++)
+            {
+                string line = "";
+                Console.Write(string.Format("{0,38}", ""));
+
+                if (r / 4 <= x)
+                {
+                    prog++;
+                }
+                for (int y = 0; y < row_star.Length; y++)
+                {
+                    if (r / 4 == x | r * 3 / 4 == x | col - 1 == y | temp - 1 - x == y | (y == prog | y == row_star.Length - 1 - prog))
+                    {
+
+                        Console.ForegroundColor = randomColor;
+                        Console.Write("*");
+                        line += "*";
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                        line += " ";
+                    }
+                }
+                Console.WriteLine();
+
+                int div = row < 7 ? 2 : 3;
+                document.Add(new Paragraph(new string(' ', int.Parse(43.ToString()) / div) + line, new Font(Font.FontFamily.COURIER, 12, Font.NORMAL, BaseColor.BLUE)));
+
+            }
+            document.Close();
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            StarDavid(new Random().Next(3, 9));
+        }
+    }
+}
---
+++ TempleLampShape/TempleLampShape/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace TempleLampShape
+{
+    internal class Program
+    {
+
+        static void TempleLamp()
+        {
+
+            //Prints the temple lamp
+            //*   *   *   *   *   *   *   *   *
+            // *   *   *   *  *  *   *   *   * 
+            //  *   *   *   * * *   *   *   *  
+            //   *   *   *   ***   *   *   *   
+            //    *   *   *   *   *   *   *    
+            //     *   *   *  *  *   *   *     
+            //      *   *   * * *   *   *      
+            //       *   *   ***   *   *       
+            //        *   *   *   *   *        
+            //         *   *  *  *   *         
+            //          *   * * *   *          
+            //           *   ***   *           
+            //            *   *   *            
+            //             *  *  *             
+            //              * * *              
+            //               ***               
+            //                *
+            //                *
+            //                *
+            //                *
+            //              * * *
+            //             * * * *
+
+
+            int row = 21;
+            int col = 33;
+            int temp = col;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            // to save to file pdf Temple Lamp shape
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream("TempleOfLamp.pdf", FileMode.Create));
+            document.Open();
+
+            // Add title to the PDF document
+            string title = "Temple of Lamp";
+            Paragraph titleParagraph = new Paragraph(title, new Font(Font.FontFamily.COURIER, 18, Font.BOLD, BaseColor.BLACK));
+            titleParagraph.Alignment = Element.ALIGN_CENTER;
+            StringBuilder outputBuilder = new StringBuilder();
+            document.Add(titleParagraph);
+            Chunk emptyLine = new Chunk("\n");
+            document.Add(emptyLine);
+
+            for (int i = 0; i < row; i++)
+            {
+                Paragraph lineParagraph = null;
+                Console.Write(String.Format("{0,45}", ""));
+                int space = i < 16 ? i : 16;
+                string patt = string.Format("{0," + space + "}", "") + "*";
+                for (int j = 1; j < temp / 2; j++)
+                {
+                    patt += j % 4 == 0 ? "*" : " ";
+                }
+                temp = temp % 4 == 0 ? col : temp - 2;
+                char[] charArray = patt.ToCharArray();
+                if (i < 16)
+                {
+                    Console.Write(patt + "*");
+
+                    Array.Reverse(charArray);
+                    Console.WriteLine(charArray);
+                    outputBuilder.AppendLine(new string(' ', 17) + patt + "*" + new string(charArray));
+                    lineParagraph = new Paragraph(new string(' ', 17) + patt + "*" + new string(charArray), new Font(Font.FontFamily.COURIER, 12, Font.NORMAL, BaseColor.BLUE));
+                }
+                else
+                {
+                    Console.WriteLine(patt);
+                    lineParagraph = new Paragraph(new string(' ', 17) + patt, new Font(Font.FontFamily.COURIER, 12, Font.NORMAL, BaseColor.BLUE));
+                    outputBuilder.AppendLine(new string(' ', 17) + patt);
+                }
+
+                document.Add(lineParagraph);
+            }
+            File.WriteAllText("TempleOfLamp.txt", outputBuilder.ToString());
+            document.Close();
+        }
+        static void Main(string[] args)
+        {
+
+            TempleLamp();
+        }
+    }
+}
