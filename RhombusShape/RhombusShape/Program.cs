0780f1165ca917340a14c52a086d9d0735e51132
elad770 <73057751+elad770@users.noreply.github.com>
2023-06-25 00:06:16 +0300

    update project RhombusShape

--- RhombusShape/RhombusShape/Program.cs
+++ RhombusShape/RhombusShape/Program.cs
@@ -11,6 +11,19 @@ namespace RhombusShape
 {
     internal class Program
     {
+        /*
+          A program that shows some functions with techniques 
+          for printing a rhombus on the screen
+                      *
+                     ***
+                    *****   
+                   *******
+                  *********
+                   *******                                    
+                    *****  
+                     ***
+                      *
+         */
 
         public static void RecoDrawRhombus(int rows)
         {
@@ -117,16 +130,59 @@ namespace RhombusShape
             }
         }
 
+        public static void DrawRhombusSimple2(int rows)
+        {
+            //A function that draws a rhombus in a fairly simple way, the idea is to
+            //create a normal triangle in the opposite way and save it and 
+            //while printing the normal triangle and then print the inverted triangle
+
+            string sign = "*";
+            string triangles = "";
+            string t = new string('\t', 6);
+            rows = rows % 2 == 0 ? rows + 1 : rows;
+            for (int i = 0; i <= rows / 2; i++)
+            {
+                string format = string.Format("{0," + (rows / 2 - i) + "}", "");
+                Console.WriteLine(t + format + sign);
+
+                // Create a string of triangles in reverse
+                triangles += '\n' + sign + format + t;
+
+                // triangles += t + format + sign + '\n';
+                sign += "**";
+            }
+
+            // Running from the end to the beginning - 8 character: this 1 the
+            // last character, 6 is of '\t' and 1 of '\n'
+            for (int i = triangles.Length - (rows + 8); i >= 0; i--)
+            {
+                Console.Write(triangles[i]);
+            }
+
+            // A second way, instead of running through all the characters 
+            // in the string itself, you can split the string according to the \n character
+            // into lines and run from the end of the array to the beginning (note that 
+            // you need to run from the third term from the end and not from the second!
+            // and the string of the triangle in the first loop in the usual order )
+
+            /*string[] spilt = triangles.Split('\n');
+            for (int i = spilt.Length - 3; i >= 0; i--)
+            {
+                Console.WriteLine(spilt[i]);
+            }*/
+
+        }
 
         static void Main(string[] args)
         {
             Console.WriteLine();
+            DrawRhombusSimple2(new Random().Next(4, 23));
             Console.ForegroundColor = ConsoleColor.Blue;
             DrawRhombus(new Random().Next(4, 23));
             Console.ForegroundColor = ConsoleColor.Red;
             DrawRhombusSimple(new Random().Next(4, 23));
             Console.ForegroundColor = ConsoleColor.Cyan;
-            RecoDrawRhombus(new Random().Next(10, 23));
+            RecoDrawRhombus(new Random().Next(4, 23));
         }
     }
 }
